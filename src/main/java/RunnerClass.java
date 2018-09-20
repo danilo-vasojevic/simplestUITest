fb20597562efd169b827484589e711b5f7ec3284
Danilo Vasojevic <danilo.m.vasojevic@gmail.com>
2018-09-20 11:23:18 +0200

    Initial commit

---
+++ src/main/java/RunnerClass.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class RunnerClass  {
+    public static void main(String[] args) throws MalformedURLException {
+
+        // Capabilities initialization
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("68.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        // WebDriver initialization
+        RemoteWebDriver driver = new RemoteWebDriver(
+                URI.create("http://localhost:4444/wd/hub").toURL(),
+                capabilities
+        );
+
+        // Test
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Cheese!");
+        element.submit();
+        System.out.println("Page title is: " + driver.getTitle());
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!");
+            }
+        });
+        System.out.println("Page title is: " + driver.getTitle());
+        //driver.quit();
+    }
+}
