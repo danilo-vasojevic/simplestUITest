389802f51f18586e066e6228ac83d5d01c0a8fe5
Danilo Vasojevic <danilo.m.vasojevic@gmail.com>
2018-09-27 14:38:30 +0200

    Ignore this commit

--- src/main/java/RunnerClass.java
+++ src/main/java/RunnerClass.java
@@ -3,14 +3,10 @@ import Tests.GoogleSearch;
 import java.net.MalformedURLException;
 
 public class RunnerClass {
-
-    public static void main(String[] args) {
-        try {
-            new GoogleSearch(WebDriverProvider.initChrome69()).start();
-            new GoogleSearch(WebDriverProvider.initFirefox62()).start();
-            new GoogleSearch(WebDriverProvider.initChrome68()).start();
-            new GoogleSearch(WebDriverProvider.initFirefox61()).start();
-        } catch (MalformedURLException ignored) {
-        }
+    public static void main(String[] args) throws MalformedURLException {
+        new GoogleSearch(WebDriverProvider.initChrome69()).start();
+        new GoogleSearch(WebDriverProvider.initFirefox62()).start();
+        new GoogleSearch(WebDriverProvider.initChrome68()).start();
+        new GoogleSearch(WebDriverProvider.initFirefox61()).start();
     }
 }
